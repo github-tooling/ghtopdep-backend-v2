f7071d43d5da913b4c403410643747abb538b107
Andrii Oriekhov <andriyorehov@gmail.com>
2022-05-19 19:07:43 +0300

    add route to fetch all repos

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require('path');
+const AutoLoad = require('fastify-autoload');
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(require('@fastify/cors'), {
+    // put your options here
+  });
 
   // Do not touch the following lines
 
@@ -13,13 +16,13 @@ module.exports = async function (fastify, opts) {
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    options: Object.assign({}, opts),
+  });
+};
--- routes/repos/index.js
+++ routes/repos/index.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 const { NotFound } = require('http-errors');
-const Redis = require("ioredis");
-require('dotenv').config()
+const Redis = require('ioredis');
+require('dotenv').config();
 
-const ONE_MONTH = 60*60*24*30;
+const ONE_MONTH = 60 * 60 * 24 * 30;
 
 const redis = new Redis({
   host: process.env.REDIS_HOST,
@@ -12,20 +12,23 @@ const redis = new Redis({
   password: process.env.REDIS_PASSWORD,
 });
 
-
 module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
-    const redisKey = `${request.body.owner}:${request.body.repository}`
-    await redis.set(redisKey, JSON.stringify(request.body.deps), "EX", ONE_MONTH)
-    return "OK";
+    const redisKey = `${request.body.owner}:${request.body.repository}`;
+    await redis.set(redisKey, JSON.stringify(request.body.deps), 'EX', ONE_MONTH);
+    return 'OK';
   });
 
   fastify.get('/', async function (request, reply) {
-    if (request.query.owner &&  request.query.repository) {
-      const redisKey = `${request.query.owner}:${request.query.repository}`
+    return redis.keys('*');
+  });
+
+  fastify.get('/:owner/:repository', async function (request, reply) {
+    if (request.params.owner && request.params.repository) {
+      const redisKey = `${request.params.owner}:${request.params.repository}`;
       const result = await redis.get(redisKey);
       if (result) {
-        return result
+        return result;
       }
     }
     throw new NotFound();
