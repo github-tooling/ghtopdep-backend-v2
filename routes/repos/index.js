5d77848d3bcfa49cd0456267b99cf47b88de8119
Andrii Oriekhov <andriyorehov@gmail.com>
2022-05-19 17:01:21 +0300

    cleanup

--- routes/repos/index.js
+++ routes/repos/index.js
@@ -15,20 +15,15 @@ const redis = new Redis({
 
 module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
-    console.log(request.body)
     const redisKey = `${request.body.owner}:${request.body.repository}`
     await redis.set(redisKey, JSON.stringify(request.body.deps), "EX", ONE_MONTH)
     return "OK";
   });
 
   fastify.get('/', async function (request, reply) {
-    console.log(request.query);
     if (request.query.owner &&  request.query.repository) {
-      console.log(request.query.url);
       const redisKey = `${request.query.owner}:${request.query.repository}`
       const result = await redis.get(redisKey);
-      console.log('result');
-      console.log(result);
       if (result) {
         return result
       }
